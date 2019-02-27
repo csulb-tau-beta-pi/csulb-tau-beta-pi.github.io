a7f5a7a9ba7136d6321644804e33a9d3fb1abb02
Airrick_ <32466687+Airrick102@users.noreply.github.com>
2019-02-26 23:09:42 -0800

    First commit

---
+++ precache-manifest.bc7eee3d2674cec0a0aff0ffd71494d2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6438de12d02ecbc85b7d80314f44c74",
+    "url": "/static/media/nikkinguyen.c6438de1.jpg"
+  },
+  {
+    "revision": "d7671d55b53588a9130a",
+    "url": "/static/css/main.5e61df18.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "57cf05dc35ddeec0ad17259e467c4cf6",
+    "url": "/static/media/minhkhoavu.57cf05dc.jpg"
+  },
+  {
+    "revision": "a53f15ccb741810e8dec",
+    "url": "/static/js/2.a53f15cc.chunk.js"
+  },
+  {
+    "revision": "6aecd77f593b96472ab9b58af9869893",
+    "url": "/static/media/NexaDemo-Bold.6aecd77f.otf"
+  },
+  {
+    "revision": "8e16c92f593f77ff8527f8aa468eb4f0",
+    "url": "/static/media/NexaDemo-Light.8e16c92f.otf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/static/media/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "99d396377ec12286acbbf68fff25d2bf",
+    "url": "/static/media/small-logo-chapter-yellow-white.99d39637.png"
+  },
+  {
+    "revision": "b9ffacc0b30f333d0ba160a207d9b88a",
+    "url": "/static/media/initiation-f2018.b9ffacc0.jpg"
+  },
+  {
+    "revision": "9df007c7830ef7fae0979e8201ce466c",
+    "url": "/static/media/harrisonfrank.9df007c7.jpg"
+  },
+  {
+    "revision": "a6fef5d77c3b24d9922566911dbeba00",
+    "url": "/static/media/chrissam.a6fef5d7.jpg"
+  },
+  {
+    "revision": "d7671d55b53588a9130a",
+    "url": "/static/js/main.d7671d55.chunk.js"
+  },
+  {
+    "revision": "dcf9ab8ef671975a9a909f4f68dd8a3b",
+    "url": "/static/media/anthonytruong.dcf9ab8e.jpg"
+  },
+  {
+    "revision": "7ef196ea5d4025a99f6a36bac25819ff",
+    "url": "/static/media/matthewchu.7ef196ea.jpg"
+  },
+  {
+    "revision": "693a8a494fe7337322acaf055dc5619a",
+    "url": "/static/media/ryanvalera.693a8a49.jpg"
+  },
+  {
+    "revision": "b24715ef2c3d14a04b33acb954ec2abe",
+    "url": "/static/media/jeffreynguyen.b24715ef.jpg"
+  },
+  {
+    "revision": "fa5412106bdfad55f89d8ff3382eacd6",
+    "url": "/static/media/madisonkane.fa541210.jpg"
+  },
+  {
+    "revision": "7c1eabbdc4506c5d873174879e9adf15",
+    "url": "/static/media/ericdo.7c1eabbd.jpg"
+  },
+  {
+    "revision": "2221f74ca86859a801c504dcbbf28d72",
+    "url": "/static/media/nicholasnguyen.2221f74c.jpg"
+  },
+  {
+    "revision": "1baa2734d1c0ebcbd45a4a7a99bf3f0d",
+    "url": "/static/media/evelynmuro.1baa2734.jpg"
+  },
+  {
+    "revision": "41106b937d5f4c08a95faa3a02ba03fa",
+    "url": "/static/media/seanblydthansen.41106b93.jpg"
+  },
+  {
+    "revision": "a53f15ccb741810e8dec",
+    "url": "/static/css/2.281ccff0.chunk.css"
+  },
+  {
+    "revision": "0293b9c53476a5d37081983cae9caeb5",
+    "url": "/index.html"
+  }
+];
